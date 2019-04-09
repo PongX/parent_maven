1d52f28cee8c7ea24d65f0c33b791218e62e704a
PongX <1076361663@qq.com>
2019-04-09 19:00:36 +0800

    第一次提交到git

---
+++ maven_controller/src/main/java/test/HelloWorld.java
@@ -0,0 +1,20 @@
+package test;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+		System.out.println("更新。。。");
+        System.out.println("更新2。。。");
+		System.out.println("更新3。。。");
+		int a=10;
+        int b=30;
+		String y="2";
+		int c=3;
+		int d=8;
+		String r="3";
+		int w=6;
+		int o=9;
+		String u="9";
+		int p=9;
+    }
+}
